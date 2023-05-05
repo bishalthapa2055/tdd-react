13668ef8abf7a383ac83b71d03d007af61e830a3
bishalthapa2055 <bishalthapa2055@gmail.com>
2023-05-05 16:54:31 +0545

    intital test

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -3,7 +3,18 @@ import React from 'react'
 const SignUpPage = () => {
   return (
     <>
-    <h1>Sign Up</h1>
+    <div>
+        <h1>Sign Up</h1>
+        <input id="username" />
+        <label htmlFor="username">Username</label>
+        <input id="email" />
+        <label htmlFor="email">E-mail</label>
+        <input id="password" type="password" />
+        <label htmlFor="password">Password</label>
+        <input id="passwordRepeat" type="password" />
+        <label htmlFor="passwordRepeat">Password Repeat</label>
+        <button disabled>Sign Up</button>
+      </div>
     </>
   )
 }
--- src/pages/SignUpPage.test.js
+++ src/pages/SignUpPage.test.js
@@ -1,12 +1,65 @@
+
 import SignUpPage from "./SignUpPage";
 import {render , screen} from  "@testing-library/react"
 
-describe("Sign UP Page" , () =>{
+describe("inital render of signup page" , () =>{
 
     test("Initial Render",()=>{
         
         render(<SignUpPage />);
         const header = screen.queryByRole('heading',{name:/sign up/i});
         expect(header).toBeInTheDocument()
+    })
+})
+
+describe("Initital condition of sinnup page" ,()=>{
+
+
+    const renderFun = () =>{
+        render(<SignUpPage />)
+    }
+    test("should have h1 tag",()=>{
+            renderFun()
+            const header = screen.queryByRole("heading",{name:/sign up/i});
+            expect(header).toBeInTheDocument();
+            expect(header).toBeVisible();
+
+    })
+    test("should have username field",()=>{
+        renderFun()
+        const username = screen.getByLabelText(/username/i);
+        expect(username).toBeInTheDocument();
+    })
+    test("should have email field",()=>{
+        renderFun()
+        const email = screen.queryByLabelText(/e-mail/i);
+        expect(email).toBeInTheDocument();
+    })
+    test("should have password field",()=>{
+        renderFun();
+        const password = screen.getByLabelText("Password");
+        expect(password).toBeInTheDocument();
+        expect(password).toHaveProperty('type','password')
+    })
+
+    test("for repeat password",()=>{
+        renderFun();
+        const repeat =screen.queryByLabelText("Password Repeat");
+        expect(repeat).toBeInTheDocument();
+        expect(repeat).toBeVisible();
+        expect(repeat.type).toBe('password')
+
+    })
+    test("for button",()=>{
+        renderFun();
+        const button = screen.queryByRole('button',{name:"Sign Up"});
+        expect(button).toBeVisible();
+        expect(button).toBeInTheDocument()
+    })
+
+    test("inital condition of button",()=>{
+        renderFun();
+        const button = screen.queryByRole('button',{name :/sign up/i});
+        expect(button).toBeDisabled()
     })
 })
