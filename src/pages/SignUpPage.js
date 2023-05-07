2f7f6f3561485109853d54a50bbb7e72e71d5d47
Bishal Thapa <bishalthapab989@gmail.com>
2023-05-07 11:24:01 +0545

    basic api call

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const SignUpPage = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordRepeat, setPasswordRepeat] = useState("");
 
@@ -11,13 +14,25 @@ const SignUpPage = () => {
     }
     return isDisabled;
   };
+  const submit = async (e) => {
+    e.preventDefault();
+    const body = {
+      username,
+      email,
+      password,
+      passwordRepeat,
+    };
+    const response = await axios.post("/api/1.0/user", body);
+    console.log(response);
+  };
+
   return (
     <>
-      <div>
+      <form>
         <h1>Sign Up</h1>
-        <input id="username" />
+        <input id="username" onChange={(e) => setUsername(e.target.value)} />
         <label htmlFor="username">Username</label>
-        <input id="email" />
+        <input id="email" onChange={(e) => setEmail(e.target.value)} />
         <label htmlFor="email">E-mail</label>
         <input
           id="password"
@@ -31,8 +46,10 @@ const SignUpPage = () => {
           onChange={(e) => setPasswordRepeat(e.target.value)}
         />
         <label htmlFor="passwordRepeat">Password Repeat</label>
-        <button disabled={disabled()}>Sign Up</button>
-      </div>
+        <button disabled={disabled()} onClick={submit}>
+          Sign Up
+        </button>
+      </form>
     </>
   );
 };
--- src/pages/SignUpPage.test.js
+++ src/pages/SignUpPage.test.js
@@ -1,6 +1,7 @@
 import userEvent from "@testing-library/user-event";
 import SignUpPage from "./SignUpPage";
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
 
 describe("inital render of signup page", () => {
   test("Initial Render", () => {
@@ -74,5 +75,44 @@ describe("Interactions", () => {
     await userEvent.type(password, "Bishal");
     await userEvent.type(passwordRepeat, "Bishal");
     expect(button).toBeEnabled();
+
+    await userEvent.type(password, "hello there");
+    await userEvent.type(passwordRepeat, "bishal");
+    expect(button).toBeDisabled();
+  });
+
+  test("For an api call", async () => {
+    renderFun();
+    const username = screen.queryByLabelText("Username");
+    const email = screen.queryByLabelText("E-mail");
+    const password = screen.queryByLabelText("Password");
+    const passwordRepeat = screen.queryByLabelText("Password Repeat");
+    expect(username).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(passwordRepeat).toBeInTheDocument();
+
+    const button = screen.queryByRole("button", { name: /sign up/i });
+    expect(button).toBeDisabled();
+
+    await userEvent.type(username, "Bishal");
+    await userEvent.type(email, "admin@gmail.com");
+    await userEvent.type(password, "Hello");
+    await userEvent.type(passwordRepeat, "Hello");
+
+    expect(button).toBeEnabled();
+
+    const mockFn = jest.fn();
+    axios.post = mockFn;
+
+    await userEvent.click(button);
+    const firstCallofMockFunction = mockFn.mock.calls[0];
+    const body = firstCallofMockFunction[1];
+    expect(body).toEqual({
+      username: "Bishal",
+      email: "admin@gmail.com",
+      password: "Hello",
+      passwordRepeat: "Hello",
+    });
   });
 });
